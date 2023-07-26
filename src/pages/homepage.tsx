fef34f89e63a08578cfceec5f90fa2f1058a03bc
Kabina Thapa <thapakabina28@gmail.com>
2023-07-26 00:19:34 -0400

    Update Homepage UI

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -13,14 +13,21 @@ const homepage = () => {
   return (
     <>
     
-    <div className='bg-stone-300 w-full'>
+    <div className='bg-stone-300 w-full h-auto'>
     <Navbar/>
-      <section className='w-[70%] h-full bg-stone-50 mx-auto'>
-        <img className='ml-auto' src={img} width='450px'/>
+      <section className='w-[70%] h-auto bg-stone-50 mx-auto flex'>
+        <div className='p-6 w-96 grid grid-rows-3 place-items-center text-lg'>
+          <h1 className='text-7xl'>Winter 21'</h1>
+          <p>Winter layer season is here. Check out our trendy new winter collection to stay warm in style.</p>
+          <button className='border-2 w-full p-2'>Shop Now</button>
+        </div>
+       
+        <img className='relative right-[-8%] ml-auto' src={img} width='450px'/>
+        
       </section>
       
     </div>
-    <div className='w-full  bg-stone-300'>
+    <div className='w-full  bg-stone-300 flex'>
     <section className='w-[70%] h-[90%] mx-auto pt-2'>
     <h1 className='text-3xl'>Our Category</h1>
     <div className=' flex mt-4 border-2 p-4 justify-around'>
