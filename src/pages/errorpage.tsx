24a82fa861aafa92d4ef099db46f21070cb15df6
Kabina Thapa <thapakabina28@gmail.com>
2023-07-31 15:30:53 -0400

    Merge with main branch and update in UI of homepage

--- src/pages/errorpage.tsx
+++ src/pages/errorpage.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import{useRouteError} from "react-router-dom"
+import Navbar from '../components/navbar'
+import '../App.css'
 
 const errorpage = () => {
   const error=useRouteError()
   console.log(error)
 
   return (
-    <div className='bg-stone-300 w-full h-screen'>
-    <div id='errorpage' >
-      <h1>OOPs!</h1>
-      <p>
+    
+    <div className='bg-stone-300 w-full h-screen '>
+    <Navbar/>
+    <div id='errorpage' className=' mx-auto w-[70%] text-center' >
+      <h1 className='word text-8xl font-bold mb-4 '>OOPs!</h1>
+      <p className='text-2xl mb-2'>
         404-Page <i>{error.statusText || error.message}</i>
       </p>
-      <p>The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+      <p className='text-xl'>The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
     </div>
     </div>
   )
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -6,8 +6,10 @@ import {
 } from "../features/productSlice";
 
 import Navbar from "./../components/navbar";
+import Footer from "../components/footer";
 import { Link } from "react-router-dom";
 import img from "../assets/de17b27a-image-1.png";
+import App from '../App.css'
 
 const homepage = () => {
   //const {data:products}=useGetProductsQuery()
@@ -19,26 +21,26 @@ const homepage = () => {
       <div className="bg-stone-300 w-full h-auto">
         <Navbar />
         <section className="w-[75%] h-auto bg-stone-50 mx-auto flex">
-          <div className="p-8 w-96 grid grid-rows-3 ">
-            <h1 className="text-6xl text-slate-900 mt-8">Winter 21'</h1>
-            <p className="text-slate-600 mb-6 text-sm ">
+          <div className="p-8 w-96 grid grid-rows-3 gap-4 ">
+            <h1 className="text-7xl text-slate-900 mt-8">Winter 21'</h1>
+            <p className="text-slate-600 mb-6 text-xl ">
               Winter layer season is here. Check out our trendy new winter
               collection to stay warm in style.
             </p>
-            <button className=" mt-2 mb-24 mr-20 border-stone-500 text-sm border-2 hover:bg-stone-500 hover:border-none hover:text-white">
+            <button className=" h-14 border-stone-400 text-sm border-2 hover:bg-stone-500 hover:border-none hover:text-white">
               Shop Now
             </button>
           </div>
 
           <img
-            className="relative right-[-8%] ml-auto mt-5"
+            className="relative right-[-6%] ml-auto mt-5"
             src={img}
             width="450px"
           />
         </section>
-      </div>
+      
       <div className="w-full  bg-stone-300 flex">
-        <section className="w-[70%] h-[90%] mx-auto pt-2 m-28">
+        <section className="w-[75%] h-auto mx-auto p-8 pb-12 m-12 bg-white">
           <h1 className="text-3xl">Our Category</h1>
           <div className=" flex mt-12 border-2 p-4 justify-around">
             {data &&
@@ -55,8 +57,13 @@ const homepage = () => {
                 </ul>
               ))}
           </div>
+          
         </section>
+        
       </div>
+      <Footer/>
+      </div>
+      
     </>
   );
 };
